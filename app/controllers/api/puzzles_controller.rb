3700c295def4a8622f9a5c384314a5181f88fe5b
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-05-05 08:30:39 -0400

    Add Puzzle show action

--- app/controllers/api/puzzles_controller.rb
+++ app/controllers/api/puzzles_controller.rb
@@ -7,5 +7,10 @@ module Api
       end
       render json: puzzles, status: 200
     end
+
+    def show
+      puzzle = Puzzle.find(params[:id])
+      render json: puzzle, status: 200
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   namespace :api, path: '/', constraints: { subdomain: 'api'} do
-    resources :puzzles, only: :index
+    resources :puzzles, only: [:index, :show]
   end
 end
