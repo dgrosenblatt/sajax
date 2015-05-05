b8d9e9f5ddb2eeaa8aa64739a9fed2409f2fc9a0
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-05-05 08:26:22 -0400

    Add Puzzle index action with category filter

--- app/controllers/api/puzzles_controller.rb
+++ app/controllers/api/puzzles_controller.rb
@@ -1,6 +1,11 @@
 module Api
   class PuzzlesController < ApplicationController
     def index
+      puzzles = Puzzle.all
+      if category = params[:category]
+        puzzles = puzzles.where("lower(category) = ?", category.downcase)
+      end
+      render json: puzzles, status: 200
     end
   end
 end
