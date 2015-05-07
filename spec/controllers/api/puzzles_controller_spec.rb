899bf9d0fe5e5a8d33eaf29ec48dcab34f13a7be
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-05-07 17:19:27 -0400

    Add xml format to puzzles#show

--- spec/controllers/api/puzzles_controller_spec.rb
+++ spec/controllers/api/puzzles_controller_spec.rb
@@ -1,37 +1,46 @@
 require 'rails_helper'
-require 'pry'
 
 describe Api::PuzzlesController do
-  before(:each) { @request.host = "api.example.com" }
+  before(:each) { @request.host = 'api.example.com' }
 
-  describe "GET #index" do
+  describe 'GET #index' do
     before { FactoryGirl.create_list(:puzzle, 5) }
 
-    it "renders JSON representation of puzzles" do
-      @request.headers['Accept'] = "application/json"
+    it 'renders JSON representation of puzzles' do
+      @request.headers['Accept'] = Mime::JSON
       get :index
       puzzle_response = JSON.parse(response.body, symbolize_names: true)
       expect(response.status).to eq 200
-      expect(response.content_type).to eq "application/json"
+      expect(response.content_type).to eq Mime::JSON.to_s
       expect(puzzle_response[:puzzles].length).to eq 5
     end
 
-    it "renders XML representation of puzzles" do
-      @request.headers['Accept'] = "application/xml"
+    it 'renders XML representation of puzzles' do
+      @request.headers['Accept'] = Mime::XML
       get :index
       expect(response.status).to eq 200
-      expect(response.content_type).to eq "application/xml"
+      expect(response.content_type).to eq Mime::XML.to_s
     end
   end
 
-  describe "GET #show" do
-    it "returns information about a puzzle" do
+  describe 'GET #show' do
+    it 'renders JSON representation of a puzzle' do
       puzzle = FactoryGirl.create(:puzzle)
+      @request.headers['Accept']  = Mime::JSON
       get :show, id: puzzle.id
       puzzle_response = JSON.parse(response.body, symbolize_names: true)
-      expect(response).to be_success
+      expect(response.status).to eq 200
       expect(puzzle_response[:puzzle][:solution]).to eq puzzle.solution
       expect(puzzle_response[:puzzle][:category]).to eq puzzle.category
+      expect(response.content_type).to eq Mime::JSON.to_s
+    end
+
+    it 'renders XML representation of a puzzle' do
+      puzzle = FactoryGirl.create(:puzzle)
+      @request.headers['Accept']  = Mime::XML
+      get :show, id: puzzle.id
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq Mime::XML.to_s
     end
   end
 end
